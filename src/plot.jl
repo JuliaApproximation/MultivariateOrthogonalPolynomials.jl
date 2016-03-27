21bcb3cd8c79a29fd66be5a2673389c914670352
Sheehan Olver <solver@mac.com>
2016-03-28 09:05:52 +1100

    Remove ambiguity warnings

--- src/plot.jl
+++ src/plot.jl
@@ -1,5 +1,7 @@
 ## Disk specific
-function Plots.surface{S,V,SS<:DiskSpace}(f::ProductFun{S,V,SS};opts...)
+function Plots.surface{S<:UnivariateSpace,
+                       V<:UnivariateSpace,
+                       SS<:DiskSpace}(f::ProductFun{S,V,SS};opts...)
     x,y,vals=points(f,1),points(f,2),real(values(f))
     surface([x x[:,1]],[y y[:,1]],[vals vals[:,1]];opts...)
 end
