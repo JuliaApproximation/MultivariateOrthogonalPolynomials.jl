4869731e2a5b191efd3f433ef43303e2c0f34322
Sheehan Olver <solver@mac.com>
2017-01-19 15:05:26 +1100

    advection diffusion on a triangle with GLPlot

--- examples/Advection-diffusion.jl
+++ examples/Advection-diffusion.jl
@@ -2,68 +2,12 @@ using ApproxFun, MultivariateOrthogonalPolynomials, FixedSizeArrays, Plots, GLVi
     import MultivariateOrthogonalPolynomials: plan_evaluate
 
 
-PT = ProductTriangle(1,1,1)
-    X, Y = points(PT,10,10)
-    X = Float32.(X)
-    Y = Float32.(Y)
+window = glscreen()
+
+@async GLWindow.waiting_renderloop(window)
+
 
 S = TriangleWeight(1,1,1,KoornwinderTriangle(1,1,1))
-u0=10Fun(S,randn(10));
-    P = plan_evaluate(u0)
-    Z = Matrix{Float32}(P.(X,Y))
-
-
-
-window = glscreen()
-_view(visualize((X,Y,Z), :surface))
-@async renderloop(window)
-
-plot(pad(u0,100))
-
-f=Fun((x,y)->exp(-x^2-y^2*sin(y)),Chebyshev()^2,100)
-plot(f)
-ncoefficients(f)
-
-f=pad(f,105)
-pts = points(f)
-plot(first.(pts),last.(pts),values(f))
-
-
-ApproxFun.nblocks(f)
-
-pts = points(f)
-values(f)
-ApproxFun.coefficientmatrix(f)
-surface(first.(pts),last.(pts),va
-
-f.(pts)
-
-@which points(space(f),10)
-
-@time ApproxFun.itransform(space(f),coefficients(f))
-
-
-surface(u0)
-
-@profile values(u0)
-
-Profile.print()
-
-pts=points(u0)
-
-P = MultivariateOrthogonalPolynomials.plan_evaluate(u0)
-
-
-@time P.plan.(pts)
-
-
-
-@time
-
-ApproxFun.weight.(S,[Vec(0.1,0.2),Vec(0.1,0.2)])
-
-P(pts)
-
 
 Δ = Laplacian(S)
 Dx = Derivative(S,[1,0])
@@ -76,3 +20,36 @@ h=0.001
 A=I-h*(ε*Δ+Dx)
 
 QR=qrfact(A)
+C=cache(eye(A))  # conversion operator
+
+
+# Plotting grid
+PT = ProductTriangle(1,1,1)
+    X, Y = points(PT,30,30)
+    X = Float32.(X)
+    Y = Float32.(Y)
+
+X = linspace(0f0,1f0,30)*ones(Float32,30)'
+Y = X'
+Y = (1-X).*Y
+
+
+# Initial condition
+u0=Fun(S,4randn(10));
+
+
+
+u=u0
+P = plan_evaluate(u)
+Z = Matrix{Float32}(P.(X,Y))
+vis = visualize((X,Y,Z), :surface)
+_view(vis)
+
+for k=1:100
+    u=\(QR,C*u;tolerance=1E-5)
+    chop!(u,1E-7)
+    P = plan_evaluate(u)
+    Z = Matrix{Float32}(P.(X,Y))
+    GLAbstraction.set_arg!(vis, :position_z, Z)
+    yield()
+end
