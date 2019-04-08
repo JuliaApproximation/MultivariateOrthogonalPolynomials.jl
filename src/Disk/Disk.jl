b4c1b998586fa5483b063770a6ead0a537ba6f59
Sheehan Olver <solver@mac.com>
2019-04-08 16:57:05 +0100

    start tests

--- src/Disk/Disk.jl
+++ src/Disk/Disk.jl
@@ -20,6 +20,30 @@ plan_transform(S::ChebyshevDisk, n::AbstractVector) =
 plan_itransform(S::ChebyshevDisk, n::AbstractVector) = 
     ITransformPlan(S, plan_itransform(rectspace(S),n), Val{false})
 
+# drop every other entry
+function checkerboard(A::AbstractMatrix{T}) where T
+    m,n = size(A)
+    C = zeros(T, (m+1)÷2, n)
+    z = @view(A[1:2:end,1])
+    e1 = @view(A[1:2:end,4:4:end])
+    e2 = @view(A[1:2:end,5:4:end])
+    o1 = @view(A[2:2:end,2:4:end])
+    o2 = @view(A[2:2:end,3:4:end])
+    C[1:size(z,1),1] .= z
+    C[1:size(e1,1),4:4:n] .= e1
+    C[1:size(e2,1),5:4:n] .= e2
+    C[1:size(o1,1),2:4:n] .= o1
+    C[1:size(o2,1),3:4:n] .= o2
+    C
+end
+
+function icheckerboard(C::AbstractMatrix{T}) where T
+    m,n = size(C)
+    A = zeros(T, 2*m, n)
+    A[1:2:end,1:2:end] .= C[:,1:2:end]
+    A[2:2:end,2:2:end] .= C[:,2:2:end]
+    A
+end
 
 *(P::TransformPlan{<:Any,<:ChebyshevDisk}, v::AbstractArray) = checkerboard(P.plan*v)
 *(P::ITransformPlan{<:Any,<:ChebyshevDisk}, v::AbstractArray) = P.plan*icheckerboard(v)
--- test/test_disk.jl
+++ test/test_disk.jl
@@ -1,3 +1,9 @@
-using ApproxFun
+using Revise, ApproxFun, MultivariateOrthogonalPolynomials
+import MultivariateOrthogonalPolynomials: checkerboard, icheckerboard
+
 f = (x,y) -> x*y+cos(y-0.1)+sin(x)+1; ff = Fun((r,θ) -> f(r*cos(θ),r*sin(θ)), (-1..1) × PeriodicSegment());
-ApproxFunBase.coefficientmatrix(ff)
+
+cfs = ApproxFunBase.coefficientmatrix(ff)
+icheckerboard(checkerboard(cfs))[1:size(cfs,1),:]
+
+cfs
