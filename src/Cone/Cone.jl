09d78a109180f971b34aef247d354da784b1223d
Sheehan Olver <solver@mac.com>
2019-04-28 09:04:43 +0100

    Cone -> Conic

--- src/Cone/Cone.jl
+++ src/Cone/Cone.jl
@@ -1,6 +1,153 @@
-export Cone, DuffyCone, LegendreCone
+export Cone, DuffyCone, LegendreCone, Conic, DuffyConic, LegendreConic
+
+### 
+#  Conic
+###
+
+struct Conic <: Domain{Vec{3,Float64}} end
+struct DuffyConic <: Space{Conic,Float64} end
+struct LegendreConic <: Space{Conic,Float64} end
+
+@containsconstants DuffyConic
+@containsconstants LegendreConic
+
+spacescompatible(::DuffyConic, ::DuffyConic) = true
+spacescompatible(::LegendreConic, ::LegendreConic) = true
+
+domain(::DuffyConic) = Conic()
+domain(::LegendreConic) = Conic()
+
+tensorizer(K::DuffyConic) = DiskTensorizer()
+tensorizer(K::LegendreConic) = DiskTensorizer()
+
+rectspace(::DuffyConic) = NormalizedJacobi(0,1,Segment(1,0))*Fourier()
+
+conemap(t,x,y) = Vec(t, t*x, t*y)
+conemap(txy) = ((t,x,y) = txy; conemap(t,x,y))
+conicpolar(t,θ) = conemap(t, cos(θ), sin(θ))
+conicpolar(tθ) = ((t,θ) = tθ; conicpolar(t,θ))
+
+points(::Conic, n) = conicpolar.(points(rectspace(DuffyConic()), n))
+checkpoints(::Conic) = conicpolar.(checkpoints(rectspace(DuffyConic())))
+
+
+plan_transform(S::DuffyConic, n::AbstractVector) = 
+    TransformPlan(S, plan_transform(rectspace(S),n), Val{false})
+plan_itransform(S::DuffyConic, n::AbstractVector) = 
+    ITransformPlan(S, plan_itransform(rectspace(S),n), Val{false})
+
+*(P::TransformPlan{<:Any,<:DuffyConic}, v::AbstractArray) = P.plan*v
+*(P::ITransformPlan{<:Any,<:DuffyConic}, v::AbstractArray) = P.plan*v
+
+evaluate(cfs::AbstractVector, S::DuffyConic, txy) = evaluate(cfs, rectspace(S), ipolar(txy[Vec(2,3)]))
+evaluate(cfs::AbstractVector, S::LegendreConic, txy) = evaluate(coefficients(cfs, S, DuffyConic()), DuffyConic(), txy)
+
+
+# function transform(::DuffyConic, v)
+#     n = (1 + isqrt(1+8*length(v))) ÷ 4
+#     F = copy(reshape(v,n,2n-1))
+#     Pt = plan_transform(NormalizedJacobi(0,1,Segment(1,0)), n)
+#     Pθ = plan_transform(Fourier(), 2n-1)
+#     for j = 1:size(F,2)
+#         F[:,j] = Pt*F[:,j]
+#     end
+#     for k = 1:size(F,1)
+#         F[k,:] = Pθ * F[k,:]
+#     end
+#     fromtensor(rectspace(DuffyConic()), F)
+# end
+
+function evaluate(cfs::AbstractVector, S::DuffyConic, txy::Vec{3})
+    t,x,y = txy
+    @assert t ≈ sqrt(x^2+y^2)
+    θ = atan(y,x)
+    Fun(rectspace(S), cfs)(t,θ)
+end
+
+
+function _coefficients(triangleplan, v::AbstractVector{T}, ::DuffyConic, ::LegendreConic) where T
+    F = totensor(rectspace(DuffyConic()), v)
+    F = pad(F, :, 2size(F,1)-1)
+    Fc = F[:,1:2:end]
+    c_execute_tri_lo2hi(triangleplan, Fc)
+    F[:,1:2:end] .= Fc
+    # ignore first column
+    Fc[:,2:end] .= F[:,2:2:end]
+    c_execute_tri_lo2hi(triangleplan, Fc)
+    F[:,2:2:end] .= Fc[:,2:end]
+    fromtensor(LegendreConic(), F)
+end
+
+function _coefficients(triangleplan, v::AbstractVector{T}, ::LegendreConic,  ::DuffyConic) where T
+    F = totensor(LegendreConic(), v)
+    F = pad(F, :, 2size(F,1)-1)
+    Fc = F[:,1:2:end]
+    c_execute_tri_hi2lo(triangleplan, Fc)
+    F[:,1:2:end] .= Fc
+    # ignore first column
+    Fc[:,2:end] .= F[:,2:2:end]
+    c_execute_tri_hi2lo(triangleplan, Fc)
+    F[:,2:2:end] .= Fc[:,2:end]
+    fromtensor(DuffyConic(), F)
+end
+
+function coefficients(cfs::AbstractVector{T}, CD::DuffyConic, ZD::LegendreConic) where T
+    c = totensor(rectspace(DuffyConic()), cfs)            # TODO: wasteful
+    n = size(c,1)
+    _coefficients(c_plan_rottriangle(n, zero(T), zero(T), zero(T)), cfs, CD, ZD)
+end
+
+function coefficients(cfs::AbstractVector{T}, ZD::LegendreConic, CD::DuffyConic) where T
+    c = tridevec(cfs)            # TODO: wasteful
+    n = size(c,1)
+    _coefficients(c_plan_rottriangle(n, zero(T), zero(T), zero(T)), cfs, ZD, CD)
+end
+
+struct LegendreConicTransformPlan{DUF,CHEB}
+    duffyplan::DUF
+    triangleplan::CHEB
+end
+
+function LegendreConicTransformPlan(S::LegendreConic, v::AbstractVector{T}) where T 
+    D = plan_transform(DuffyConic(),v)
+    F = totensor(rectspace(DuffyConic()), D*v) # wasteful, just use to figure out `size(F,1)`
+    P = c_plan_rottriangle(size(F,1), zero(T), zero(T), zero(T))
+    LegendreConicTransformPlan(D,P)
+end
+
+
+*(P::LegendreConicTransformPlan, v::AbstractVector) = _coefficients(P.triangleplan, P.duffyplan*v, DuffyConic(), LegendreConic())
+
+plan_transform(K::LegendreConic, v::AbstractVector) = LegendreConicTransformPlan(K, v)
+
+struct LegendreConicITransformPlan{DUF,CHEB}
+    iduffyplan::DUF
+    triangleplan::CHEB
+end
+
+function LegendreConicITransformPlan(S::LegendreConic, v::AbstractVector{T}) where T 
+    F = fromtensor(LegendreConic(), v) # wasteful, just use to figure out `size(F,1)`
+    P = c_plan_rottriangle(size(F,1), zero(T), zero(T), zero(T))
+    D = plan_itransform(DuffyConic(), _coefficients(P, v, S, DuffyConic()))
+    LegendreConicITransformPlan(D,P)
+end
+
+
+*(P::LegendreConicITransformPlan, v::AbstractVector) = P.iduffyplan*_coefficients(P.triangleplan, v, LegendreConic(), DuffyConic())
+
+
+plan_itransform(K::LegendreConic, v::AbstractVector) = LegendreConicITransformPlan(K, v)
+
+
+
+### 
+#  Cone
+###
 
 struct Cone <: Domain{Vec{3,Float64}} end
+
+in(x::Vec{3}, d::Cone) = 0 ≤ x[1] ≤ 1 && x[2]^2 + x[3]^2 ≤ x[1]^2 
+
 struct DuffyCone <: Space{Cone,Float64} end
 struct LegendreCone <: Space{Cone,Float64} end
 
@@ -13,16 +160,16 @@ spacescompatible(::LegendreCone, ::LegendreCone) = true
 domain(::DuffyCone) = Cone()
 domain(::LegendreCone) = Cone()
 
-tensorizer(K::DuffyCone) = DiskTensorizer()
-tensorizer(K::LegendreCone) = DiskTensorizer()
+tensorizer(K::DuffyCone) = BallTensorizer()
+tensorizer(K::LegendreCone) = BallTensorizer()
 
-rectspace(::DuffyCone) = NormalizedJacobi(0,1,Segment(1,0))*Fourier()
+rectspace(::DuffyCone) = NormalizedJacobi(0,1,Segment(1,0))*ZernikeDisk()
 
-conepolar(t,θ) = Vec(t, t*cos(θ), t*sin(θ))
-conepolar(tθ) = ((t,θ) = tθ; conepolar(t,θ))
 
-points(::Cone, n) = conepolar.(points(rectspace(DuffyCone()), n))
-checkpoints(::Cone) = conepolar.(checkpoints(rectspace(DuffyCone())))
+
+
+points(::Cone, n) = conemap.(points(rectspace(DuffyCone()), n))
+checkpoints(::Cone) = conemap.(checkpoints(rectspace(DuffyCone())))
 
 
 plan_transform(S::DuffyCone, n::AbstractVector) = 
@@ -53,9 +200,8 @@ evaluate(cfs::AbstractVector, S::LegendreCone, txy) = evaluate(coefficients(cfs,
 
 function evaluate(cfs::AbstractVector, S::DuffyCone, txy::Vec{3})
     t,x,y = txy
-    @assert t ≈ sqrt(x^2+y^2)
-    θ = atan(y,x)
-    Fun(rectspace(S), cfs)(t,θ)
+    @assert x^2+y^2 ≤ t^2
+    Fun(rectspace(S), cfs)(t,x/t,y/t)
 end
 
 
--- test/test_cone.jl
+++ test/test_cone.jl
@@ -1,63 +1,65 @@
 using ApproxFun, MultivariateOrthogonalPolynomials, Test
 import MultivariateOrthogonalPolynomials: rectspace, totensor
+import ApproxFunBase: plan_transform
 
-@testset "DuffyCone" begin
-    f = Fun((t,x,y) -> 1, DuffyCone(), 10)
-    @test f.coefficients ≈ [1; zeros(ncoefficients(f)-1)]
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1
+@testset "Conic" begin
+    @testset "DuffyConic" begin
+        f = Fun((t,x,y) -> 1, DuffyConic(), 10)
+        @test f.coefficients ≈ [1; zeros(ncoefficients(f)-1)]
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1
 
-    f = Fun((t,x,y) -> t, DuffyCone(), 10)
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ sqrt(0.1^2+0.2^2)
+        f = Fun((t,x,y) -> t, DuffyConic(), 10)
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ sqrt(0.1^2+0.2^2)
 
-    f = Fun((t,x,y) -> x, DuffyCone(), 10)
+        f = Fun((t,x,y) -> x, DuffyConic(), 10)
 
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 0.1
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 0.1
 
-    f = Fun((t,x,y) -> exp(cos(t*x)*y), DuffyCone(), 1000)
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ exp(cos(sqrt(0.1^2+0.2^2)*0.1)*0.2)
+        f = Fun((t,x,y) -> exp(cos(t*x)*y), DuffyConic(), 1000)
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ exp(cos(sqrt(0.1^2+0.2^2)*0.1)*0.2)
 
-    f = Fun((t,x,y) -> exp(cos(t*x)*y), DuffyCone())
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ exp(cos(sqrt(0.1^2+0.2^2)*0.1)*0.2)
+        f = Fun((t,x,y) -> exp(cos(t*x)*y), DuffyConic())
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ exp(cos(sqrt(0.1^2+0.2^2)*0.1)*0.2)
 
-    m,ℓ = (1,1)
-    f = (txy) -> ((t,x,y) = txy;  θ = atan(y,x); Fun(NormalizedJacobi(0,2m+1,Segment(1,0)),[zeros(ℓ);1])(t) * 2^m * t^m * cos(m*θ))
-    g = Fun(f, DuffyCone())
-    t,x,y = sqrt(0.1^2+0.2^2),0.1,0.2
-    @test g(t,x,y) ≈ f((t,x,y))
-end
+        m,ℓ = (1,1)
+        f = (txy) -> ((t,x,y) = txy;  θ = atan(y,x); Fun(NormalizedJacobi(0,2m+1,Segment(1,0)),[zeros(ℓ);1])(t) * 2^m * t^m * cos(m*θ))
+        g = Fun(f, DuffyConic())
+        t,x,y = sqrt(0.1^2+0.2^2),0.1,0.2
+        @test g(t,x,y) ≈ f((t,x,y))
+    end
 
-@testset "LegendreConePlan" begin
-    m,ℓ = (1,1)
-    f = (txy) -> ((t,x,y) = txy;  θ = atan(y,x); Fun(NormalizedJacobi(0,2m+1,Segment(1,0)),[zeros(ℓ);1])(t) * 2^m * t^m * cos(m*θ))
-    p = points(LegendreCone(), 10)
-    P = plan_transform(LegendreCone(), f.(p))
-    @test P.duffyplan*f.(p) ≈ Fun(f, DuffyCone()).coefficients[1:12]
-    coefficients(g, LegendreCone())
-    g = Fun(f, LegendreCone(), 20)
-    t,x,y = sqrt(0.1^2+0.2^2),0.1,0.2
-    @test g(t,x,y) ≈ f((t,x,y))
-end
+    @testset "LegendreConicPlan" begin
+        m,ℓ = (1,1)
+        f = (txy) -> ((t,x,y) = txy;  θ = atan(y,x); Fun(NormalizedJacobi(0,2m+1,Segment(1,0)),[zeros(ℓ);1])(t) * 2^m * t^m * cos(m*θ))
+        p = points(LegendreConic(), 10)
+        P = plan_transform(LegendreConic(), f.(p))
+        @test P.duffyplan*f.(p) ≈ Fun(f, DuffyConic()).coefficients[1:12]
+        g = Fun(f, LegendreConic(), 20)
+        t,x,y = sqrt(0.1^2+0.2^2),0.1,0.2
+        @test g(t,x,y) ≈ f((t,x,y))
+    end
 
-@testset "Legendre<>DuffyCone" begin
-    a = randn(10)
-    F = totensor(rectspace(DuffyCone()), a)
-
-
-    b = coefficients(a, LegendreCone(), DuffyCone())
-
-    coefficients(b, DuffyCone(), LegendreCone())
-end
-
-@testset "LegendreCone" begin
-    f = Fun((t,x,y) -> 1, LegendreCone(), 10)
-    @test f.coefficients ≈ [1; zeros(ncoefficients(f)-1)]
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1
-
-    f = Fun((t,x,y) -> t, LegendreCone(), 10)
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ sqrt(0.1^2+0.2^2)
-    f = Fun((t,x,y) -> 1+t+x+y, LegendreCone(), 10)
-    @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1+sqrt(0.1^2+0.2^2)+0.1+0.2
+    @testset "Legendre<>DuffyConic" begin
+        a = randn(10)
+        F = totensor(rectspace(DuffyConic()), a)
 
 
-    @time Fun((t,x,y) -> 1+t+x+y, LegendreCone(), 1000)
+        b = coefficients(a, LegendreConic(), DuffyConic())
+
+        coefficients(b, DuffyConic(), LegendreConic())
+    end
+
+    @testset "LegendreConic" begin
+        f = Fun((t,x,y) -> 1, LegendreConic(), 10)
+        @test f.coefficients ≈ [1; zeros(ncoefficients(f)-1)]
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1
+
+        f = Fun((t,x,y) -> t, LegendreConic(), 10)
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ sqrt(0.1^2+0.2^2)
+        f = Fun((t,x,y) -> 1+t+x+y, LegendreConic(), 10)
+        @test f(sqrt(0.1^2+0.2^2),0.1,0.2) ≈ 1+sqrt(0.1^2+0.2^2)+0.1+0.2
+
+
+        @time Fun((t,x,y) -> 1+t+x+y, LegendreConic(), 1000)
+    end
 end
