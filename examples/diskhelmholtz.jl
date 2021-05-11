c8247459a1ce4cfdcf13c938b1fcb392f890848b
Sheehan Olver <solver@mac.com>
2021-05-11 23:04:18 +0100

    HarmonicOrthogonalPolynomials-v0.1

--- examples/diskhelmholtz.jl
+++ examples/diskhelmholtz.jl
@@ -1,80 +1,18 @@
 using MultivariateOrthogonalPolynomials, FastTransforms, BlockBandedMatrices, Plots
 plotly()
-import MultivariateOrthogonalPolynomials: ZernikeITransform, grid, plotgrid
 
+Z = Zernike(1)
+W = Weighted(Z)
+xy = axes(Z,1); x,y = first.(xy),last.(xy)
+Δ = Z \ (Laplacian(xy) * W)
+S = Z \ W
+k = 2
+f = @.(cos(x*exp(y)))
+F = factorize(Δ + k^2 * S)
+c = (Z \ f)
+F \ c
 
-Z = Zernike()[:,Block.(Base.OneTo(10))]
-g = grid(Z)
-θ = getproperty.(g[1,:],:θ)
-[permutedims(RadialCoordinate.(1,θ)); g; permutedims(RadialCoordinate.(0,θ))]
-import Mul
+u = W * ((Δ + k^2 * S) \ (Z \ f))
 
-plotgrid(Z)
-
-
-Z = Zernike()
-xy = axes(Z,1)
-x,y = first.(xy),last.(xy)
-u = Z * (Z \ @.(cos(10x*y)))
-surface(u)
-
-@.(cos(10x*y))
-
-plot(u)
-
-g = plotgrid(Z[:,Block.(Base.OneTo(10))])
-surface(first.(g), last.(g), u[g])
-plot(u)
-
-
-G = grid(Z)
-
-contourf(first.(G), last.(G), ones(size(G)...))
-
-
-scatter(vec(first.(G)), vec(last.(G)))
-G[1,1].θ
-
-WZ = Weighted(Zernike(1))
-xy = axes(WZ,1)
-x,y = first.(xy),last.(xy)
-f = Zernike(1) \ @. exp(x*cos(y))
-
-N = 50
-KR = Block.(Base.OneTo(N))
-Δ = (Zernike(1) \ (Laplacian(xy) * WZ))[KR,KR]
-C = (Zernike(1) \ WZ)[KR,KR]
-k = 5
-L = Δ - k^2 * C
-
-v = f[KR]
-@time u = (L \ v);
-
-g = MultivariateOrthogonalPolynomials.grid(Zernike(1)[:,KR])
-U = ZernikeITransform{Float64}(N, 0, 1) * u
-
-plot(first.(g), last.(g), U)
-
-
-
-F = factorize(Zernike(1)[:,KR]).plan
-F \ u
-u
-
-
-F*u
-
-
-F.plan \ v
-F |> typeof |> fieldnames
-
-grid(WZ[:,KR])
-
-F |>typeof |> fieldnames
-F.F * v
-
-m = DiskTrav(v).matrix
-
-plan_disk2cxf(m, 0, 0) * m
 
 
