3441329413665279e14a72756e7732118e9eb8e9
Sheehan Olver <solver@mac.com>
2021-05-20 19:52:11 +0100

    fix indenting

--- examples/diskhelmholtz.jl
+++ examples/diskhelmholtz.jl
@@ -7,25 +7,6 @@ xy = axes(Z,1); x,y = first.(xy),last.(xy)
 Δ = Z \ (Laplacian(xy) * W)
 S = Z \ W
 
-KR = Block.(Base.OneTo(5)); @time S.args[1][KR,KR];
-@time aB = S.args[1].ops[1]; kr = 1:500; 
-@time aB[kr,kr];
-bc = Base.broadcasted(view(aB,kr,kr))
-@time copy(bc)
-@ent aB.args[1][kr,kr] ./ aB.args[2]
-aB.f
-@time aB[kr,kr]
-
-AssertionErro
-@time aB.args[1][kr];
-@time aB[kr,kr];
-@
-using ArrayLayouts
-c = aB[kr,kr]
-
-@time copyto!(c, bc);
-@time aB[kr,kr];
-@time aB.args[2][kr,kr];
 k = 2
 f = @.(cos(x*exp(y)))
 F = factorize(Δ + k^2 * S)
--- src/ModalInterlace.jl
+++ src/ModalInterlace.jl
@@ -2,14 +2,13 @@
 ModalInterlace
 """
 struct ModalInterlace{T, MMNN<:Tuple} <: AbstractBandedBlockBandedMatrix{T}
-ops
-MN::MMNN
-bandwidths::NTuple{2,Int}
+    ops
+    MN::MMNN
+    bandwidths::NTuple{2,Int}
 end
 
-ModalInterlace{T}(ops, MN::NTuple{2,Integer}, bandwidths::NTuple{2,Int}) where T = 
-ModalInterlace{T,typeof(MN)}(ops, MN, bandwidths)
-
+ModalInterlace{T}(ops, MN::NTuple{2,Integer}, bandwidths::NTuple{2,Int}) where T = ModalInterlace{T,typeof(MN)}(ops, MN, bandwidths)
+ModalInterlace(ops::AbstractVector{<:AbstractMatrix{T}}, MN::NTuple{2,Integer}, bandwidths::NTuple{2,Int}) where T = ModalInterlace{T}(ops, MN, bandwidths)
 
 axes(Z::ModalInterlace) = blockedrange.(oneto.(Z.MN))
 
@@ -18,23 +17,23 @@ subblockbandwidths(::ModalInterlace) = (0,0)
 
 
 function Base.view(R::ModalInterlace{T}, KJ::Block{2}) where T
-K,J = KJ.n
-dat = Matrix{T}(undef,1,J)
-l,u = blockbandwidths(R)
-if iseven(J-K) && -l ≤ J - K ≤ u
-    sh = (J-K)÷2
-    if isodd(K)
-        k = K÷2+1
-        dat[1,1] = R.ops[1][k,k+sh]
+    K,J = KJ.n
+    dat = Matrix{T}(undef,1,J)
+    l,u = blockbandwidths(R)
+    if iseven(J-K) && -l ≤ J - K ≤ u
+        sh = (J-K)÷2
+        if isodd(K)
+            k = K÷2+1
+            dat[1,1] = R.ops[1][k,k+sh]
+        end
+        for m in range(2-iseven(K); step=2, length=J÷2-max(0,sh))
+            k = K÷2-m÷2+isodd(K)
+            dat[1,m] = dat[1,m+1] = R.ops[m+1][k,k+sh]
+        end
+    else
+        fill!(dat, zero(T))
     end
-    for m in range(2-iseven(K); step=2, length=J÷2-max(0,sh))
-        k = K÷2-m÷2+isodd(K)
-        dat[1,m] = dat[1,m+1] = R.ops[m+1][k,k+sh]
-    end
-else
-    fill!(dat, zero(T))
-end
-_BandedMatrix(dat, K, 0, 0)
+    _BandedMatrix(dat, K, 0, 0)
 end
 
 getindex(R::ModalInterlace, k::Integer, j::Integer) = R[findblockindex.(axes(R),(k,j))...]
