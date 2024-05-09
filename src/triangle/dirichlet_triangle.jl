47d512bcf4a8e3065c1035d73d9271452c04bc96
DanielVandH <danj.vandenheuvel@gmail.com>
2024-05-09 16:49:40 +0100

    Implement getindex

--- src/triangle/dirichlet_triangle.jl
+++ src/triangle/dirichlet_triangle.jl
@@ -38,7 +38,7 @@ function getindex(P::DirichletTriangle, xy::SVector{2}, JR::BlockOneTo)
 end
 
 function _getindex_dirichlet_1(P::DirichletTriangle, xy::SVector{2}, JR::BlockOneTo)
-    a, b, c = P.a, P.b, P.c
+    a, b, _ = P.a, P.b, P.c
     n = length(JR)
     x, y = xy
     z = 1 - x - y
@@ -82,15 +82,64 @@ function _getindex_dirichlet_2(P::DirichletTriangle, xy::SVector{2}, JR::BlockOn
     jacobi_vals = PJ[xy, JR]
     dirichlet_vals = copy(jacobi_vals)
     dirichlet_vals[1] = 1.0
-    if a && b 
-        prev_block = view(jacobi_vals, Block(1))
-    elseif a && c 
-
+    if a && b
+        P̃ = Jacobi(0.0, 1.0)[2x-1, 1:n]
+        PJ_one = JacobiTriangle(0.0, 1.0, 0.0)
+        jacobi_one_vals = PJ_one[xy, JR]
+        for i in 2:n
+            dirichlet_block = view(dirichlet_vals, Block(i))
+            dirichlet_block[1] = x * P̃[i-1]
+            dirichlet_block[end] = y * view(jacobi_one_vals, Block(i - 1))[end]
+            for k in 2:(i-1)
+                dirichlet_block[k] = x * y * view(jacobi_vals, Block(i - 2))[k-1]
+            end
+        end
+    elseif a && c
+        P̃ = Jacobi(0.0, 1.0)[2x-1, 1:n]
+        PJ_one = JacobiTriangle(0.0, 0.0, 1.0)
+        jacobi_one_vals = PJ_one[xy, JR]
+        for i in 2:n
+            dirichlet_block = view(dirichlet_vals, Block(i))
+            dirichlet_block[1] = x * P̃[i-1]
+            dirichlet_block[end] = z * view(jacobi_one_vals, Block(i - 1))[end]
+            for k in 2:(i-1)
+                dirichlet_block[k] = x * z * view(jacobi_vals, Block(i - 2))[k-1]
+            end
+        end
     else
-
+        P̃ = Jacobi(1.0, 0.0)[2x-1, 1:n]
+        for i in 2:n
+            dirichlet_block = view(dirichlet_vals, Block(i))
+            dirichlet_block[1] = (1 - x) * P̃[i-1]
+            dirichlet_block[2] = (1 - x - 2y) * P̃[i-1]
+            for k in 3:i
+                dirichlet_block[k] = y * z * view(jacobi_vals, Block(i - 2))[k-2]
+            end
+        end
     end
+    return dirichlet_vals
 end
 
 function _getindex_dirichlet_3(P::DirichletTriangle, xy::SVector{2}, JR::BlockOneTo)
-    a, b, c = P.a, P.b, P.c
+    n = length(JR)
+    x, y = xy 
+    z = 1 - x - y
+    dirichlet_vals = PseudoBlockVector(zeros(n * (n - 1) ÷ 2 + n), 1:n)
+    dirichlet_vals[1] = 1.0
+    n ==1 && return dirichlet_vals
+    dirichlet_vals[2] = 1.0 - 2.0x
+    dirichlet_vals[3] = 1.0 - x - 2.0y
+    P¹¹_vals = Jacobi(1.0, 1.0)[x, 1:n]
+    P¹¹¹_vals = JacobiTriangle(1.0, 1.0, 1.0)[xy, JR]
+    P⁰¹¹_vals = JacobiTriangle(0.0, 1.0, 1.0)[xy, JR]
+    for i in 3:n
+        dirichlet_block = view(dirichlet_vals, Block(i))
+        dirichlet_block[1] = x * (1 - x) * P¹¹_vals[i-2]
+        dirichlet_block[2] = x * (1.0 - x - 2.0y) * P¹¹_vals[i-2]
+        dirichlet_block[end] = y * z * view(P⁰¹¹_vals, Block(i - 2))[end]
+        for k in 3:(i-1)
+            dirichlet_block[k] = x * y * z * view(P¹¹¹_vals, Block(i - 3))[k-2]
+        end
+    end
+    return dirichlet_vals
 end
--- test/test_dirichlettriangle.jl
+++ test/test_dirichlettriangle.jl
@@ -76,7 +76,7 @@ function Qₙₖᵃᵇᶜf(n, k, a, b, c, x, y)
         elseif k == 1
             return (1 - x - 2y) * Pₙₖf(n - 1, 0, x, y)
         else
-            return y * z * Pₙₖᵃᵇᶜ(n - 2, k - 2, 0, 1, 1, x, y)
+            return y * z * Pₙₖᵃᵇᶜf(n - 2, k - 2, 0, 1, 1, x, y)
         end
     elseif a == 1 && b == 1 && c == 1
         if n == k == 0
@@ -86,48 +86,25 @@ function Qₙₖᵃᵇᶜf(n, k, a, b, c, x, y)
         elseif n == k == 1
             return 1 - x - 2y
         elseif k == 0
-            return x * (1 - x) * Pₙₖᵃᵇᶜf(n - 2, 0, 1, 0, 0, x, y)
+            # return x * (1 - x) * Pₙₖᵃᵇᶜf(n - 2, 0, 1, 0, 0, x, y) # Not the same as x(1-x)Pₙ₋₂¹¹(x)?
+            return x * (1 - x) * Pₙᵃᵇf(n - 2, 1, 1, x)
         elseif k == 1
-            return x * (1 - x - 2y) * Pₙₖᵃᵇᶜf(n - 2, 0, 1, 0, 0, x, y)
+            # return x * (1 - x - 2y) * Pₙₖᵃᵇᶜf(n - 2, 0, 1, 0, 0, x, y)
+            return x * (1 - x - 2y) * Pₙᵃᵇf(n - 2, 1, 1, x)
         elseif n ≠ k
             return x * y * z * Pₙₖᵃᵇᶜf(n - 3, k - 2, 1, 1, 1, x, y)
         else
-            return y * z * Pₙₖᵅᵇᶜf(n - 2, n - 2, 0, 1, 1, x, y)
+            return y * z * Pₙₖᵃᵇᶜf(n - 2, n - 2, 0, 1, 1, x, y)
         end
     else
         throw(ArgumentError("Invalid values for a, b, c"))
     end
 end
-for (a, b, c) in ((1, 0, 0), (0, 1, 0), (0, 0, 1))
+for (a, b, c) in ((1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (0, 1, 1), (1, 1, 1))
+    @show a, b, c
     P = DirichletTriangle(a, b, c)
     xy = SVector(0.2, 0.3)
     vals = P[xy, BlockRange(1:10)]
     Qvals = [Qₙₖᵃᵇᶜf(to_nk(i)..., a, b, c, xy...) for i in 1:to_idx(10 - 1, 10 - 1)]
     @test vals ≈ Qvals
 end
-
-a, b, c = 0, 1, 0
-P = DirichletTriangle(a, b, c)
-xy = SVector(0.2, 0.3)
-vals = P[xy, 17]
-vals ≈ Qₙₖᵃᵇᶜf(to_nk(17)..., a, b, c, xy[1], xy[2])
-
-P[xy, 1], Qₙₖᵃᵇᶜf(to_nk(1)..., a, b, c, xy[1], xy[2])
-P[xy, 2], Qₙₖᵃᵇᶜf(to_nk(2)..., a, b, c, xy[1], xy[2])
-
-P[xy, 3], Qₙₖᵃᵇᶜf(to_nk(3)..., a, b, c, xy[1], xy[2])
-
-
-JR = findblockindex(axes(P, 2), 3) |> BlockArrays.block |> x -> Block.(Base.OneTo(Int(x)))
-a, b, c = P.a, P.b, P.c
-n = length(JR)
-x, y = xy
-PJ = JacobiTriangle(P)
-jacobi_vals = PJ[xy, JR]
-dirichlet_vals = copy(jacobi_vals)
-P̃ = Jacobi(0.0, 0.0)[2x-1, 1:n]
-prev_block = view(jacobi_vals, Block(1))
-mult = b ? y : z
-i = 2
-block = view(jacobi_vals, Block(i))
-dirichlet_block = view(dirichlet_vals, Block(i))
