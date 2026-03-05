99e2cdffbdeff0e544b6190e85c1838c1b0ba11e
Jorge Punter <george.punter-piquer25@imperial.ac.uk>
2026-03-05 11:09:12 +0000

    Update rectangle.jl

--- examples/rectangle.jl
+++ examples/rectangle.jl
@@ -70,7 +70,6 @@ for n=1:N
                 k+=1
                 ret += c[Block(n)[iter]] * P[0.1,i] * P[0.2,j] * P[0.3,k]
                 iter+=1
-                
             end
         end
 end
