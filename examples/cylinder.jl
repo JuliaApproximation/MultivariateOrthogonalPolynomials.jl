34a424fd7953334bebc46b062a18a234204d26c3
Sheehan Olver <solver@mac.com>
2026-06-29 13:59:45 +0100

    CompatHelper -> Dependabot

--- examples/cylinder.jl
+++ examples/cylinder.jl
@@ -28,40 +28,29 @@ F_ipl = inv(F_pl)
 
 
 
-volumeslice()
 
-x
-y
-z
-F
+X = range(-1, 1, 500)
+Y = range(-1, 1, 500)
+Z = range(-1, 1, 500)
 
-vol = [
+F = [
     (x^2 + y^2 <= 1) ? sin(3z+10x) * exp(-(x^2 + y^2)) : NaN
     # sin(3z) * exp(-(x^2 + y^2)) 
-    for x in x, y in y, z in z
-]
+    for x in X, y in Y, z in Z
+];
+# F = [x^2 + y^2 ≤ 1 ? Zernike()[SVector(x,y),Block(1):(M+1)] : NaN for x in X, y in Y]
+# F = vol;
 
 fig = Figure(;size=(1000,1000))
 ax = LScene(fig[1, 1], show_axis=false)
-
-X = range(-1, 1, 100)
-Y = range(-1, 1, 100)
-Z = range(-1, 1, 100)
-
-M
-
-[x^2 + y^2 ≤ 1 ? Zernike()[SVector(x,y),Block(1):(M+1)] : NaN for x in X, y in Y]
-
-[ for x in X, y in Y]
-
-plt = volumeslices!(ax, x, y, z, F; colorrange = (-1.0, 1.0))
+plt = volumeslices!(ax, X, Y, Z, F; colorrange = (-1.0, 1.0))
 
 # Typical interactive pattern with sliders
 sgrid = SliderGrid(
     fig[2, 1],
-    (label = "YZ slice (x)", range = 1:length(x)),
-    (label = "XZ slice (y)", range = 1:length(y)),
-    (label = "XY slice (z)", range = 1:length(z)),
+    (label = "YZ slice (x)", range = axes(X,1)),
+    (label = "XZ slice (y)", range = axes(Y,1)),
+    (label = "XY slice (z)", range = axes(Z,1)),
 )
 
 sl_yz, sl_xz, sl_xy = sgrid.sliders
@@ -69,3 +58,6 @@ sl_yz, sl_xz, sl_xy = sgrid.sliders
 on(sl_yz.value) do v; plt[:update_yz][](v) end
 on(sl_xz.value) do v; plt[:update_xz][](v) end
 on(sl_xy.value) do v; plt[:update_xy][](v) end
+
+
+
